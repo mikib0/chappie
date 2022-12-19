8092c482286fcc7a1b4e8aaf9be44923dd53a57a
Ibrahim Isa Jajere <ibrahimisajajere274@gmail.com>
2022-12-19 23:48:12 +0100

    send wlcm msg on /start command

--- index.js
+++ index.js
@@ -33,12 +33,18 @@ async function getChatGPTResponse(prompt) {
 
 bot.on('message', async (msg) => {
   try {
+    if (msg.text.match(/\/start/))
+      return bot.sendMessage(
+        msg.chat.id,
+        `Welcome to Chappie, a Telegram bot that uses the OpenAI API to allow users to communicate with a large language model directly on the Telegram platform. We are excited to share the first version of Chappie with you and hope you will find it a useful and engaging tool for interacting with a language model in real-time.`
+      );;
     console.log(msg.text);
     // handle incoming message
     const chatGPTResponse = await getChatGPTResponse(msg.text);
     console.log(chatGPTResponse);
     bot.sendMessage(msg.chat.id, chatGPTResponse);
   } catch (error) {
+    console.log(error.message);
     bot.sendMessage(
       msg.chat.id,
       "sorry we've messed up...please try resending your message. Mail mikibo.hamilton@aleeas.com or contact @miki_b0 on telegram if the problem persists."
