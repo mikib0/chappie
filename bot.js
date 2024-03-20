1b1dd394448bb7eaba554a6ff9da4b6939b25ea6
Ibrahim Isa Jajere <ibrahimisajajere274@gmail.com>
2024-03-20 22:50:10 +0100

    publicise

--- bot.js
+++ bot.js
@@ -12,9 +12,7 @@ if (process.env.NODE_ENV === 'dev') {
   bot = new TelegramBot(process.env.CHAPPIE_TOKEN, {
     webHook: { port: process.env.PORT },
   });
-  bot.setWebHook(
-    `https://chappie.onrender.com/bot${bot.token}`
-  );
+  bot.setWebHook(`${process.env.WEB_HOOK}${bot.token}`);
 }
 
 module.exports = bot
